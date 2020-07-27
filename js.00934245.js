4106eb0ea5d50ab47974b1b76127d60ad9402d52
Martin <office@martinpagels.de>
2020-07-27 23:56:54 +0200

    Update for gh-pages

--- js.00934245.js
+++ js.00934245.js
@@ -161,7 +161,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52823" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53880" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -338,4 +338,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js.js"], null)
-//# sourceMappingURL=/js.00934245.js.map
+//# sourceMappingURL=js.00934245.js.map
